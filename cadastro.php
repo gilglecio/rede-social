544181e5b5a68d071421e29bf71017aba9ca8163
Gilglécio Santos <gilglecio_765@hotmail.com>
2013-07-19 12:04:34 -0300

    PHP - Criando uma rede social [aula04b] - Cadastrando usuários

--- cadastro.php
+++ cadastro.php
@@ -1,3 +1,5 @@
+<?php session_start() ?>
+
 <!DOCTYPE HTML>
 <html lang="pt-br">
     <head>
@@ -29,29 +31,71 @@
                 <h1>Cadastre-se, <span>é grátis</span></h1>
 
                 <div id="formulario">
+
+                    <?php
+                    if ( isset( $_SERVER['REQUEST_METHOD']) AND $_SERVER['REQUEST_METHOD'] == 'POST') {
+
+                        extract($_POST);
+
+                        if ($nome == '' OR strlen($nome) < 4) {
+                            echo 'Escreva seu nome corretamente';
+                        } elseif ($sobrenome == '' OR strlen($sobrenome) < 6) {
+                            echo 'Escreva seu sobrenome corretamente';
+                        } elseif ( $email == '' ){
+                            echo 'Escreva seu e-mail';
+                        } elseif ( ! preg_match("/^[a-z0-9_\.\-]+@[a-z0-9_\.\-]*[a-z0-9_\-]+\.[a-z]{2,4}$/i",$email)){
+                            echo 'Este e-mail é inválido';
+                        } else {
+
+                            include ('classes/DB.class.php');
+
+                            $verificar = DB::getConn()->prepare('SELECT `id` FROM `usuarios` WHERE `email`=?');
+                            if ($verificar->execute(array($email))) {
+                                if ($verificar->rowCount()  >= 1) {
+                                    echo 'Este e-mail já esta cadastro em nosso sistema';
+                                } elseif ( $senha == '' OR strlen($senha) < 4 ) {
+                                    echo 'Digite sua senha, ela tem que ter mais de 4 caracteres';
+                                } elseif ( strtolower($captcha) <> strtolower($_SESSION['captchaCadastro']) ) {
+                                    echo 'O código digitado no captcha não corresponde com a imagem.';
+                                } else {
+
+                                    $senhaInsert = sha1($senha);
+                                    $nascimento = "$ano-$mes-$dia";
+
+                                    $inserir = DB::getConn()->prepare('INSERT INTO `usuarios` SET `email`=?, `senha`=?, `nome`=?, `sobrenome`=?, `sexo`=?, `nascimento`=?, `cadastro`=NOW() ');
+
+                                    if ( $inserir->execute(array($email, $senhaInsert, $nome, $sobrenome, $sexo, $nascimento)) ) {
+                                        header('Location: ./');
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    ?>
+
                     <form action="" name="cadastro" method="post">
                         <div>
                             <div class="inputFloat">
                                 <span>nome</span>
-                                <input type="text" name="nome" class="inputTxt">
+                                <input type="text" name="nome" class="inputTxt" value="<?php echo $nome ?>">
                             </div>
                             <div class="inputFloat">
                                 <span>sobrenome</span>
-                                <input type="text" name="sobrenome" class="inputTxt">
+                                <input type="text" name="sobrenome" class="inputTxt" value="<?php echo $sobrenome ?>">
                             </div>
                         </div>
 
                         <span class="spanHide">eu sou</span>
                         <select name="sexo" id="">
-                            <option value="masculino">Masculino</option>
-                            <option value="feminino">Feminino</option>
+                            <option <?php echo $sexo == 'masculino' ? 'selected="selected"' ?> value="masculino">Masculino</option>
+                            <option <?php echo $sexo == 'feminino' ? 'selected="selected"' ?> value="feminino">Feminino</option>
                         </select>
 
                         <span class="spanHide">data de nascimento</span>
                         <select name="dia">
                             <?php for ($d=1; $d <= 31 ; $d++) {
                                 $zero = $d<10 ? 0 : '';
-                                echo '<option value="'.$zero.$d.'">'.$zero.$d.'</option>';
+                                echo '<option '.($d==$dia ? 'selected="selected"').' value="'.$zero.$d.'">'.$zero.$d.'</option>';
                             } ?>
                         </select>
 
@@ -61,21 +105,21 @@
 
                             for ($m=1; $m <= 12 ; $m++) {
                                 $zero = $m < 10 ? 0 : '';
-                                echo '<option value="'.$zero.$m.'">'.$meses[$m].'</option>';
+                                echo '<option '.($zero.$m==$mes ? 'selected="selected"').' value="'.$zero.$m.'">'.$meses[$m].'</option>';
                             } ?>
                         </select>
 
                         <select name="ano">
                             <?php for ($a=date('Y'); $a >= (date('Y')-100) ; $a--) {
-                                echo '<option value="'.$a.'">'.$a.'</option>';
+                                echo '<option '.($a==$ano ? 'selected="selected"').' value="'.$a.'">'.$a.'</option>';
                             } ?>
                         </select>
 
                         <span class="spanHide">seu e-mail</span>
-                        <input type="text" name="email" class="inputTxt">
+                        <input type="text" name="email" class="inputTxt" value="<?php echo $email ?>">
 
                         <span class="spanHide">nova senha</span>
-                        <input type="text" name="senha" class="inputTxt">
+                        <input type="text" name="senha" class="inputTxt" value="<?php echo $senha ?>">
 
                         <span class="spanHide">verificação contra fraudes</span>
                         <div>
@@ -84,7 +128,7 @@
                             </div>
                             <div class="inputFloat">
                                 <span>digite os caracteres ao lado</span>
-                                <input type="text" name="palavra" class="inputTxt">
+                                <input type="text" name="captcha" class="inputTxt">
                             </div>
                         </div>
 
